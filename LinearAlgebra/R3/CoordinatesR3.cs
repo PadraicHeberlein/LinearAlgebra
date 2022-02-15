38ab391e726e515b78c75d23d9c97355c4e992fe
Padraic Heberlein <padraicheberlein@gmail.com>
2022-02-15 09:00:34 -0500

    Added CoordinatesR3.cs

---
+++ LinearAlgebra/R3/CoordinatesR3.cs
@@ -0,0 +1,10 @@
+﻿namespace LinearAlgebra.R3
+{
+    public static class CoordinatesR3
+    {
+        public const int X = 0;
+        public const int Y = 1;
+        public const int Z = 2;
+        public const int DIMENSIONS = 3;
+    }
+}
