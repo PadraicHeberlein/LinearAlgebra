449c45305116a2da153a5f3ac1fc25273bc5cd3d
Padraic Heberlein <padraicheberlein@gmail.com>
2022-02-15 08:58:49 -0500

    Added Constants.cs

---
+++ LinearAlgebra/R3/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinearAlgebra.R3
+{
+    public class Constants
+    {
+        public const double ZERO = 0.000001;
+        public const double DEG_15 = Math.PI / 12;
+        public const double DEG_30 = Math.PI / 6;
+        public const double DEG_36 = Math.PI / 5;
+        public const double DEG_45 = Math.PI / 4;
+        public const double DEG_60 = Math.PI / 3;
+        public const double DEG_90 = Math.PI / 2;
+        public const double DEG_180 = Math.PI;
+    }
+}
